92ba15ce981421fe9d8670b6df0857ebcc0a5637
abdullah-2021-D <abdullah.nazari_2025@ucentralasia.org>
2022-04-30 23:03:57 +0600

    updated: readme.md new content

--- README.md
+++ README.md
@@ -1,8 +1,19 @@
 # The gandoos team on mission
 
-# The following instruction is necessary to follow in order to run React App in your Machine
+# Instructions for Frontend Team collaborators
 
-## Check if you have installed the following tools latest udpates
+## 1) Setting Up React APP and clone Repo
+
+## 2) How to contribute to repo: git conventions
+
+### 1) The following instruction is necessary to follow in order to run React App in your Machine
+
+### Use Git to clone the repository in a location in your machine
+
+- git clone https://github.com/mathemagic-art/Frontend-React.git
+- cd Frontend-React
+
+### Check if you have installed the following tools latest udpates
 
 1. Nodejs
 2. npm
@@ -13,13 +24,13 @@
 
 !important: you must have the latest updates of these tools if not please update to the latest versions
 
-## make sure you are in the correct location in terminal: inside the Frontend folder if not cd to this location: then run the following command to install all the dependencies
+### make sure you are in the correct location in terminal: inside the Frontend folder if not cd to this location: then run the following command to install all the dependencies
 
 - npm install
 
 * This command should install all the dependencies needed to run react app, if you get any error in this step means you have some problem with Nodejs version or npm please check those and try again
 
-## After installing all the dependencies you must ready to go: run the following command to open the app in local host 3000 in your default browser
+### After installing all the dependencies you must ready to go: run the following command to open the app in local host 3000 in your default browser
 
 - npm start
 
@@ -30,6 +41,27 @@
 - npm run -f audit
 
 * then try to run the _ npm start _ command again and this time it will work hopefully
+
+## 2) How to Contribute: Git Conventions
+
+### How to use git
+
+- you can work on the assigned component or feature and use the following steps to push your code to repo
+
+* git add "name of the file or all"
+* git commit -m "message of commit: please read teh guidlines for commit message"
+* git push
+
+### Git commit message guidlines
+
+1. use the following keywords in each case for better readability
+
+- "added: if you have created a new component or layout or page write the name here"
+- "updated: if you have added some more features to a component write the name of comp here"
+- "fixed: if you have fixed a bug, write the name of comp and bug type here"
+- "deleted: if you have deleted any comp or feature or page write the name here"
+
+* please restrictly follow the guidlines on the commit message so we will have a fun collaboration" \_ Nazari
 
 ### stay sharp and motivated and enjoy coding with US
 
--- src/Pages/DiffCalculator.jsx
+++ src/Pages/DiffCalculator.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useState } from "react";
+import React, { useState } from "react";
 // import MethodsCard from "../Components/MethodsCard";
 import Navbar from "../Layouts/Navbar";
 import { ReactComponent as Fx } from "../Files/svgs/fx.svg";
@@ -8,7 +8,7 @@ import { ReactComponent as X2 } from "../Files/svgs/xSquare.svg";
 import FunctionsMenu from "../Layouts/FunctionsMenu";
 
 const DiffCalculator = () => {
-  const [data, setData] = useState({});
+  const [data, setData] = useState({argument_1: "", argument_2: "x", argument_3:"1"});
   const [answer, setAnswer] = useState("");
   const [isOpen, setIsOpen] = useState(false);
 
@@ -44,10 +44,12 @@ const DiffCalculator = () => {
 
   const handleReset = (event) => {
     event.preventDefault();
-    setData({ equation: "", first: 'x', second: '1' });
+    setData({ argument_1: "", argument_2: 'x', argument_3: '1' });
     setAnswer("");
   };
 
+
+  let variable = "f("+(data.argument_2)+")"
   // useEffect(() => {
 
   // }, [data, setData])
@@ -59,7 +61,7 @@ const DiffCalculator = () => {
   };
 
   const handleSubmit = (event) => {
-      axios.post("http://127.0.0.1:8000/diff/", data).then((res)=>{setAnswer(res.data)})
+      axios.post("differentiation/", data).then((res)=>{setAnswer(res.data)})
       console.log(data)
       console.log(answer)
       event.preventDefault()
@@ -86,10 +88,10 @@ const DiffCalculator = () => {
                 required
                 className="w-full p-4 border-2  border-primary rounded-l-xl text-xl"
                 type="text" 
-                name="equation" 
+                name="argument_1" 
                 id="function" 
-                value={data.equation} 
-                onChange={(e) => setData((prev) => ({ ...prev, equation: e.target.value }))}
+                value={data.argument_1} 
+                onChange={(e) => setData((prev) => ({ ...prev, argument_1: e.target.value }))}
 
                 />{" "}
                 <button className="px-4 border-2 border-primary rounded-r-xl ">
@@ -99,7 +101,7 @@ const DiffCalculator = () => {
             <label htmlFor="first" className="ml-2 text-bright">
                 With respect to variable
             </label>
-            <select name="first" id="first" value={data.first} onChange={(e) => setData((prev) => ({ ...prev, first: e.target.value }))} className="w-full p-4 border-2 text-black text-xl border-primary rounded-xl mb-10">
+            <select name="argument_2" id="first" value={data.argument_2} onChange={(e) => setData((prev) => ({ ...prev, argument_2: e.target.value }))} className="w-full p-4 border-2 text-black text-xl border-primary rounded-xl mb-10">
                 <option   value = "x" className="text-2xl">x</option>
                 <option  value = "y">y</option>
                 <option  value = "z">z</option>
@@ -107,7 +109,7 @@ const DiffCalculator = () => {
             <label htmlFor="second" className="ml-2 text-bright">
                 Order of derivative
             </label>
-            <select name="second" id="second" value={data.second} onChange={(e) => setData((prev) => ({ ...prev, second: e.target.value }))} className="w-full p-4 border-2 text-black text-xl border-primary rounded-xl mb-10">
+            <select name="argument_3" id="second" value={data.argument_3} onChange={(e) => setData((prev) => ({ ...prev, argument_3: e.target.value }))} className="w-full p-4 border-2 text-black text-xl border-primary rounded-xl mb-10">
                 <option   value = "1" className="text-2xl">First Order</option>
                 <option  value = "2">Second Order</option>
             </select>
@@ -136,10 +138,10 @@ const DiffCalculator = () => {
           <div className="flex mt-10 pl-20 pt-10 h-full w-full flex-row font-normal text-2xl tracking-wide">
             <p>
               The Derivative of{" "}
-              {!data.equation ? "f(x)" : "f(x) = " + data.equation} equals to{" "}
+              {!data.argument_1 ? variable : (variable + " = ") + data.argument_1} equals to{" "}
             </p>
             <div className="ml-3 pt-4 pb-14 border-2 font-normal rounded-xl text-3xl -mt-5 px-3 border-double border-green-600 h-10 bg-white text-black">
-              f'(x)={answer !== "" ? answer : "_____________"}
+              {data.argument_3 === "1" ? "f'" : "f''"}({data.argument_2})={answer !== "" ? answer : "_____________"}
             </div>
           </div>
         </div>
