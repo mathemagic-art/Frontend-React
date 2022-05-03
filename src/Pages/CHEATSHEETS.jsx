5b8da563be2d27bbd8c0862149d4577df89456e4
Umar Nurov <umar.nurov_2025@ucentralasia.org>
2022-05-03 20:22:09 +0600

    Cheatsheets have been started

--- src/Pages/CHEATSHEETS.jsx
+++ src/Pages/CHEATSHEETS.jsx
@@ -2,14 +2,22 @@ import React from 'react'
 import Navbar from '../Layouts/Navbar'
 import FunctionsMenu from '../Layouts/FunctionsMenu';
 
-const Cheatsheets = ({toggle, isOpen}) => {
-    return ( 
-        <div >
-        <Navbar toggle={toggle} />
-        {isOpen ? <FunctionsMenu /> : ""} 
-        <h1 className = "text-center text-white text-5xl" > This is < span > Cheatsheets </span> Page</h1 >
+
+function Cheatsheets({ toggle, isOpen }) {
+    return (
+        <div>
+            <Navbar toggle={toggle} />
+            {isOpen ? <FunctionsMenu /> : ""}
+            <h2 className="text-left mt-10 ml-60 text-3xl font-primary text-primary">
+              Basic Differentiation Rules
+            </h2>
+            <div className="ml-20 mt-5 border-2 w-[647px] h-[463px] rounded-3xl text-white p-10 bg-dark bg-opacity-30">
+            <div className="space-between first-letter:mt-5 border-2 w-[647px] h-[463px] rounded-3xl text-white p-10 bg-dark bg-opacity-30 content-end">
+
+            </div>
+            </div>
         </div>
-    )
-};
+    );
+}
 
 export default Cheatsheets;
