18fe8f3924106a7a96017b21e08a7fa6446bf5d6
Umar Nurov <umar.nurov_2025@ucentralasia.org>
2022-05-03 21:52:28 +0600

    Cheatsheets are almost done

--- src/Pages/CHEATSHEETS.jsx
+++ src/Pages/CHEATSHEETS.jsx
@@ -8,13 +8,21 @@ function Cheatsheets({ toggle, isOpen }) {
         <div>
             <Navbar toggle={toggle} />
             {isOpen ? <FunctionsMenu /> : ""}
-            <h2 className="text-left mt-10 ml-60 text-3xl font-primary text-primary">
-              Basic Differentiation Rules
-            </h2>
-            <div className="ml-20 mt-5 border-2 w-[647px] h-[463px] rounded-3xl text-white p-10 bg-dark bg-opacity-30">
-            <div className="space-between first-letter:mt-5 border-2 w-[647px] h-[463px] rounded-3xl text-white p-10 bg-dark bg-opacity-30 content-end">
+            <div className='flex justify-between mx-32 my-20'>
+                <div className='flex flex-col'>
 
-            </div>
+                    <h2 className="text-center mt-10 mb-4 text-3xl font-primary text-primary">Basic Differentiation Rules</h2>
+                    <div className="flex border-2 w-[647px] h-[463px] mb-20 rounded-3xl text-white px-10 bg-dark bg-opacity-30"></div>
+                    <h2 className="text-center mt-20 mb-4  text-3xl font-primary text-primary">Basic Differentiation Rules</h2>
+                    <div className="flex border-2 w-[647px] h-[463px] rounded-3xl text-white px-10 bg-dark bg-opacity-30 mb-20"></div>
+               </div>
+               <div className='flex flex-col'>
+               <h2 className="text-center mt-10 mb-4 text-3xl font-primary text-primary">Basic Integration Rules</h2>
+                <div className="flex border-2 w-[647px] mb-20 h-[463px] rounded-3xl text-white px-10 bg-dark bg-opacity-30 relative object-right-top"></div>
+                <h2 className="text-center mt-20 mb-4 text-3xl font-primary text-primary">Basic Limit Rules</h2>
+                <div className="flex border-2 w-[647px] h-[463px] rounded-3xl text-white px-10 bg-dark bg-opacity-30 relative object-right-top"></div>
+               </div>
+
             </div>
         </div>
     );
