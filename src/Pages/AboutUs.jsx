33fd5989024c604aba5001cb5fbdb44519c73065
Umar Nurov <umar.nurov_2025@ucentralasia.org>
2022-05-04 19:10:39 +0600

    ..

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -3,19 +3,27 @@ import Navbar from '../Layouts/Navbar'
 import FunctionsMenu from '../Layouts/FunctionsMenu'
 import { ReactComponent as Logo1 } from "../Files/svgs/Logo1.svg";
 import { ReactComponent as Quotemark } from "../Files/svgs/Quotemark.svg";
+import { ReactComponent as Muso} from "../Files/svgs/Musoagai.svg";
+import { ReactComponent as Umar} from "../Files/svgs/Umarjon.svg";
 
 const AboutUs = () => {
   return (
-    <div className='grid w-full'>
+    <div>
         <Navbar />
         <FunctionsMenu />
-          <Logo1 className="m-auto h-[134px] w-[120px] "/>
-          <Quotemark className='place-self-start ml-60 mt-5'/>
-          <h2 className="text-white text-center fon-inter ml-96 mr-96 mt-5 italic text-3xl">MatheMagic offers a wealth of smart calculators from various domains of Mathematics. The stated goal of the site is to make scientific content universally accessible by expanding the searchable data space onto scientific notations, equations, and formulas. MatheMagic, makes math simpler by doing magic.</h2>
-          <Quotemark className="rotate-180 place-self-end mr-60" />
+          <div className='grid w-full bg-white dark:bg-dark text-head dark:text-white'>
+            
+            <Logo1 className="m-auto h-[134px] w-[120px] "/>
+            <Quotemark className='place-self-start ml-60 mt-5'/>
+            <h2 className="text-center fon-inter ml-96 mr-96 mt-5 italic text-3xl">MatheMagic offers a wealth of smart calculators from various domains of Mathematics. The stated goal of the site is to make scientific content universally accessible by expanding the searchable data space onto scientific notations, equations, and formulas. MatheMagic, makes math simpler by doing magic.</h2>
+            <Quotemark className="rotate-180 place-self-end mr-60 " />
+            <Muso className="place-self-end"/>
+            <h1 className="text-center font-inter text-bold text-6xl">Meet our TEAM</h1>
+            <h2 className="text-center font-inter text-bold text-4xl mt-60">  PRODUCT MANAGERS</h2>
+            <Umar className="place-self-center mt-20 cols-2" /> 
+            <Umar className="cols-1" />
+          </div>
           
-          <h1 className="text-white text-center font-inter text-bold text-6xl">Meet our TEAM</h1>
-          <h2 className="text-white text-center font-inter text-bold text-4xl mt-60">  PRODUCT MANAGERS</h2>
     </div>
   )
 }
--- src/Pages/CHEATSHEETS.jsx
+++ src/Pages/CHEATSHEETS.jsx
@@ -3,11 +3,11 @@ import Navbar from '../Layouts/Navbar'
 import FunctionsMenu from '../Layouts/FunctionsMenu';
 
 
-function Cheatsheets() {
+function Cheatsheets({ toggle, isOpen }) {
     return (
         <div>
-            <Navbar />
-            <FunctionsMenu />
+            <Navbar toggle={toggle} />
+            {isOpen ? <FunctionsMenu /> : ""}
             <div className='flex justify-between mx-32 my-20'>
                 <div className='flex flex-col'>
 
@@ -18,9 +18,9 @@ function Cheatsheets() {
                </div>
                <div className='flex flex-col'>
                <h2 className="text-center mt-10 mb-4 text-3xl font-primary text-primary">Basic Integration Rules</h2>
-                <div className="flex border-2 w-[647px] mb-20 h-[463px] rounded-3xl text-white px-10 bg-dark bg-opacity-30"></div>
+                <div className="flex border-2 w-[647px] mb-20 h-[463px] rounded-3xl text-white px-10 bg-dark bg-opacity-30 "></div>
                 <h2 className="text-center mt-20 mb-4 text-3xl font-primary text-primary">Basic Limit Rules</h2>
-                <div className="flex border-2 w-[647px] h-[463px] rounded-3xl text-white px-10 bg-dark bg-opacity-30"></div>
+                <div className="flex border-2 w-[647px] h-[463px] rounded-3xl text-white px-10 bg-dark bg-opacity-30 relative object-right-"></div>
                </div>
 
             </div>
