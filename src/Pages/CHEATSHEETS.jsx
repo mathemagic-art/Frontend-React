2492d113d28eb44af7e19120ed0eee6060878ef0
Umidbek25 <>
2022-05-10 23:11:23 +0600

    Integration color for cheatsheets are done

--- src/Pages/CHEATSHEETS.jsx
+++ src/Pages/CHEATSHEETS.jsx
@@ -40,20 +40,20 @@ function Cheatsheets({ toggle, isOpen }) {
                     <h2 className={style.topic_calc}>Basic Differentiation Rules</h2>
                     <div className={style.box + "flex flex-col dark:bg-opacity-80"}>
                         <h4 className="ml-[40px] mt-[50px] mb-[25px] text-[18px] font-medium text-tx dark:text-primary font-inter ">Constant Rule: <ConsRule className="dark:fill-white fill-black flex flex-row ml-[130px] -mt-7"/> </h4>
-                        <h4 className="ml-[40px] mb-[20px] text-[18px] font-medium text-tx dark:text-primary font-inter ">Constant Multiple Rule: <MultRule className="flex flex-row ml-[200px] -mt-7"/> </h4>
-                        <h4 className="ml-[40px] mb-[20px] text-[18px] font-medium text-tx dark:text-primary font-inter ">Power Rule:<PowRule className="flex flex-row ml-[110px] -mt-7"/></h4>
-                        <h4 className="ml-[40px] mb-[20px] text-[18px] font-medium text-tx dark:text-primary font-inter ">Sum Rule:<SumRule className="flex flex-row ml-[95px] -mt-7"/></h4>
-                        <h4 className="ml-[40px] mb-[20px] text-[18px] font-medium text-tx dark:text-primary font-inter ">Difference Rule: <DifferenceRule className="flex flex-row ml-[140px] -mt-7"/></h4>
-                        <h4 className="ml-[40px] mb-[20px] text-[18px] font-medium text-tx dark:text-primary font-inter ">Product Rule: <ProductRule className="flex flex-row ml-[115px] -mt-7" /></h4>
-                        <h4 className="ml-[40px] mb-[20px] text-[18px] font-medium text-tx dark:text-primary font-inter ">Quotient Rule: <QuotientRule className="flex flex-row ml-[125px] -mt-7" /></h4>
-                        <h4 className="ml-[40px] mb-[45px] text-[18px] font-medium text-tx dark:text-primary font-inter ">Chain Rule:<ChainRule className="flex flex-row ml-[100px] -mt-7" /></h4>
+                        <h4 className="ml-[40px] mb-[20px] text-[18px] font-medium text-tx dark:text-primary font-inter ">Constant Multiple Rule: <MultRule className="dark:fill-white fill-black flex flex-row ml-[200px] -mt-7"/> </h4>
+                        <h4 className="ml-[40px] mb-[20px] text-[18px] font-medium text-tx dark:text-primary font-inter ">Power Rule:<PowRule className="dark:fill-white fill-black flex flex-row ml-[110px] -mt-7"/></h4>
+                        <h4 className="ml-[40px] mb-[20px] text-[18px] font-medium text-tx dark:text-primary font-inter ">Sum Rule:<SumRule className="dark:fill-white fill-black flex flex-row ml-[95px] -mt-7"/></h4>
+                        <h4 className="ml-[40px] mb-[20px] text-[18px] font-medium text-tx dark:text-primary font-inter ">Difference Rule: <DifferenceRule className="dark:fill-white fill-black flex flex-row ml-[140px] -mt-7"/></h4>
+                        <h4 className="ml-[40px] mb-[20px] text-[18px] font-medium text-tx dark:text-primary font-inter ">Product Rule: <ProductRule className="dark:fill-white fill-black flex flex-row ml-[115px] -mt-7" /></h4>
+                        <h4 className="ml-[40px] mb-[20px] text-[18px] font-medium text-tx dark:text-primary font-inter ">Quotient Rule: <QuotientRule className="dark:fill-white fill-black flex flex-row ml-[125px] -mt-7" /></h4>
+                        <h4 className="ml-[40px] mb-[45px] text-[18px] font-medium text-tx dark:text-primary font-inter ">Chain Rule:<ChainRule className="dark:fill-white fill-black flex flex-row ml-[100px] -mt-7" /></h4>
 
                     </div> 
                     <h2 className={style.topic_calc}>Basic Differentiation Rules</h2>
                     <div className={style.box + "flex flex-col bg-opacity-95 dark:bg-opacity-80"}>
-                        <h4 className="ml-[40px] mt-[45px] mb-[25px] text-[18px] font-medium text-tx dark:text-primary font-inter ">Midpoint Rule: <MidRule className="mt-[14px] mb-[10px] " /></h4>
-                        <h4 className="ml-[40px] mb-[25px] text-[18px] font-medium text-tx dark:text-primary font-inter ">Trapezoidal Rule: <TrapRule className="mt-[14px]" /></h4>
-                        <h4 className="ml-[40px] mb-[25px] text-[18px] font-medium text-tx dark:text-primary font-inter ">Simpson's Rule: <SimpRule className="mt-[14px]" /></h4>
+                        <h4 className="ml-[40px] mt-[45px] mb-[25px] text-[18px] font-medium text-tx dark:text-primary font-inter ">Midpoint Rule: <MidRule className="dark:fill-white fill-black mt-[14px] mb-[10px] " /></h4>
+                        <h4 className="ml-[40px] mb-[25px] text-[18px] font-medium text-tx dark:text-primary font-inter ">Trapezoidal Rule: <TrapRule className="dark:fill-white fill-black mt-[14px]" /></h4>
+                        <h4 className="ml-[40px] mb-[25px] text-[18px] font-medium text-tx dark:text-primary font-inter ">Simpson's Rule: <SimpRule className="dark:fill-white fill-black mt-[14px]" /></h4>
 
                     </div>
 
@@ -62,17 +62,17 @@ function Cheatsheets({ toggle, isOpen }) {
                     <h2 className={style.topic_calc}>Basic Differentiation Rules</h2>
                     <div className={style.box + "flex flex-col dark:bg-opacity-80"}>
                     <h4 className="ml-[40px] mt-[50px] mb-[20px] text-[18px] font-medium text-tx dark:text-primary font-inter ">Power Rule: <PowerRule className="dark:fill-white fill-black flex flex-row ml-[110px] -mt-9"/> </h4>
-                        <h4 className="ml-[40px] mb-[15px] text-[18px] font-medium text-tx dark:text-primary font-inter ">Exponential Rule: <ExponRule className="flex flex-row ml-[150px] -mt-9"/> </h4>
-                        <h4 className="ml-[40px] mb-[15px] text-[18px] font-medium text-tx dark:text-primary font-inter ">Exponential Rule:<ExponenRule className="flex flex-row ml-[150px] -mt-9"/></h4>
-                        <h4 className="ml-[40px] mb-[15px] text-[18px] font-medium text-tx dark:text-primary font-inter ">Trigonometry Rule:<TrigRule className="flex flex-row ml-[160px] -mt-9"/></h4>
-                        <h4 className="ml-[40px] mb-[15px] text-[18px] font-medium text-tx dark:text-primary font-inter ">Trigonometry Rule: <TrigRule1 className="flex flex-row ml-[160px] -mt-9"/></h4>
-                        <h4 className="ml-[40px] mb-[15px] text-[18px] font-medium text-tx dark:text-primary font-inter ">Inverse Trigonometry Rule: <InverseRule className="flex flex-row ml-[225px] -mt-9" /></h4>
-                        <h4 className="ml-[40px] mb-[15px] text-[18px] font-medium text-tx dark:text-primary font-inter ">Inverse Trigonometry Rule: <InverseRule1 className="flex flex-row ml-[225px] -mt-9" /></h4>   
+                        <h4 className="ml-[40px] mb-[15px] text-[18px] font-medium text-tx dark:text-primary font-inter ">Exponential Rule: <ExponRule className="dark:fill-white fill-black flex flex-row ml-[150px] -mt-9"/> </h4>
+                        <h4 className="ml-[40px] mb-[15px] text-[18px] font-medium text-tx dark:text-primary font-inter ">Exponential Rule:<ExponenRule className="dark:fill-white fill-black flex flex-row ml-[150px] -mt-9"/></h4>
+                        <h4 className="ml-[40px] mb-[15px] text-[18px] font-medium text-tx dark:text-primary font-inter ">Trigonometry Rule:<TrigRule className="dark:fill-white fill-black flex flex-row ml-[160px] -mt-9"/></h4>
+                        <h4 className="ml-[40px] mb-[15px] text-[18px] font-medium text-tx dark:text-primary font-inter ">Trigonometry Rule: <TrigRule1 className="dark:fill-white fill-black flex flex-row ml-[160px] -mt-9"/></h4>
+                        <h4 className="ml-[40px] mb-[15px] text-[18px] font-medium text-tx dark:text-primary font-inter ">Inverse Trigonometry Rule: <InverseRule className="dark:fill-white fill-black flex flex-row ml-[225px] -mt-9" /></h4>
+                        <h4 className="ml-[40px] mb-[15px] text-[18px] font-medium text-tx dark:text-primary font-inter ">Inverse Trigonometry Rule: <InverseRule1 className="dark:fill-white fill-black flex flex-row ml-[225px] -mt-9" /></h4>   
                     </div> 
                     <h2 className={style.topic_calc}>Basic Differentiation Rules</h2>
                     <div className={style.box+ "flex flex-col bg-opacity-95 dark:bg-opacity-80"}>
-                        <h4 className="ml-[40px] mt-[54px] mb-[25px] text-[18px] font-medium text-tx dark:text-primary font-inter ">Taylor's Series: <TaylsRule className="mt-[14px] mb-[40px] " /></h4>
-                        <h4 className="ml-[40px] mb-[25px] text-[18px] font-medium text-tx dark:text-primary font-inter ">Maclaurin's Series: <MacsRule className="mb-[54px]" /></h4>
+                        <h4 className="ml-[40px] mt-[54px] mb-[25px] text-[18px] font-medium text-tx dark:text-primary font-inter ">Taylor's Series: <TaylsRule className="dark:fill-white fill-black mt-[14px] mb-[40px] " /></h4>
+                        <h4 className="ml-[40px] mb-[25px] text-[18px] font-medium text-tx dark:text-primary font-inter ">Maclaurin's Series: <MacsRule className="dark:fill-white fill-black mb-[54px]" /></h4>
                     </div>
                 </div>
             
